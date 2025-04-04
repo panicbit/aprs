2e8481a864a3cf402c6ad4fbb2eaead23bb0bc2c
panicbit <panicbit.dev@gmail.com>
2025-04-04 14:40:45 +0200

    implement Deserialize for proto::server::DataPackage

--- src/proto/server/data_package.rs
+++ src/proto/server/data_package.rs
@@ -1,16 +1,16 @@
 use std::collections::BTreeMap;
 use std::sync::Arc;
 
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 
 use crate::game::HashedGameData;
 
-#[derive(Serialize, Debug)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct DataPackage {
     pub data: DataPackageData,
 }
 
-#[derive(Serialize, Debug)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct DataPackageData {
     pub games: Arc<BTreeMap<String, HashedGameData>>,
 }
