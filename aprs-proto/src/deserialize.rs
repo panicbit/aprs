d2adc00f51893399422e740ecf08bb1ab317e742
panicbit <panicbit.dev@gmail.com>
2026-01-29 04:04:43 +0100

    remove workaround for string IDs

--- aprs-proto/src/deserialize.rs
+++ aprs-proto/src/deserialize.rs
@@ -1,4 +1,4 @@
-use serde::{Deserialize, Deserializer, de};
+use serde::{Deserialize, Deserializer};
 use serde_json::Number;
 
 pub fn u128_uuid<'de, D>(de: D) -> Result<String, D::Error>
@@ -18,24 +18,5 @@ where
     match uuid {
         WeirdUuid::String(string) => Ok(string),
         WeirdUuid::Number(number) => Ok(number.to_string()),
-    }
-}
-
-pub fn i64_or_string<'de, D>(de: D) -> Result<i64, D::Error>
-where
-    D: serde::Deserializer<'de>,
-{
-    #[derive(Deserialize)]
-    #[serde(untagged)]
-    enum InternalValue {
-        Int(i64),
-        String(String),
-    }
-
-    let value = InternalValue::deserialize(de)?;
-
-    match value {
-        InternalValue::Int(n) => Ok(n),
-        InternalValue::String(s) => s.parse::<i64>().map_err(de::Error::custom),
     }
 }
--- aprs-proto/src/primitives/item_id.rs
+++ aprs-proto/src/primitives/item_id.rs
@@ -1,7 +1,5 @@
 use serde::{Deserialize, Serialize};
 
-use crate::deserialize::i64_or_string;
-
 #[derive(Serialize, Deserialize, Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 #[serde(transparent)]
-pub struct ItemId(#[serde(deserialize_with = "i64_or_string")] pub i64);
+pub struct ItemId(pub i64);
