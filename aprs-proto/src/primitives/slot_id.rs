2ddf2b4df49f7ca24dd793f7d132e205a181325f
panicbit <panicbit.dev@gmail.com>
2026-02-22 02:41:14 +0100

    proto: allow deserializing slot id from string
    SlotIds are used as key in slot_data,
    which is usually encoded as JSON.
    Since JSON keys can only be strings,
    it must be possible to decode SlotIds from strings.

--- aprs-proto/src/primitives/slot_id.rs
+++ aprs-proto/src/primitives/slot_id.rs
@@ -1,6 +1,6 @@
-use serde::{Deserialize, Serialize};
+use serde::{Deserialize, Serialize, de};
 
-#[derive(Serialize, Deserialize, Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+#[derive(Serialize, Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 #[serde(transparent)]
 pub struct SlotId(pub i64);
 
@@ -9,5 +9,24 @@ impl SlotId {
 
     pub fn is_server(&self) -> bool {
         self.0 == 0
+    }
+}
+
+impl<'de> Deserialize<'de> for SlotId {
+    fn deserialize<D>(de: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        #[derive(Deserialize)]
+        #[serde(untagged)]
+        enum IntOrString {
+            Int(i64),
+            String(String),
+        }
+
+        Ok(match IntOrString::deserialize(de)? {
+            IntOrString::Int(n) => SlotId(n),
+            IntOrString::String(n) => n.parse::<i64>().map(SlotId).map_err(de::Error::custom)?,
+        })
     }
 }
