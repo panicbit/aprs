3bfbb33bfd88438c930cc34798cca4f1338df07d
panicbit <panicbit.dev@gmail.com>
2026-02-24 21:03:47 +0100

    add slot-info tool

--- aprs-tools/src/lib.rs
+++ aprs-tools/src/lib.rs
@@ -1,18 +1,19 @@
 use color_eyre::eyre::Result;
 
 pub mod slot_data;
+pub mod slot_info;
 
 #[derive(clap::Parser)]
 pub enum Cli {
     SlotData(slot_data::Cli),
+    SlotInfo(slot_info::Cli),
 }
 
 pub fn run(cli: Cli) -> Result<()> {
     let rt = aprs_utils::default_main_setup()?;
 
-    let future = match cli {
-        Cli::SlotData(cli) => slot_data::run(cli),
-    };
-
-    rt.block_on(future)
+    match cli {
+        Cli::SlotData(cli) => rt.block_on(slot_data::run(cli)),
+        Cli::SlotInfo(cli) => rt.block_on(slot_info::run(cli)),
+    }
 }
